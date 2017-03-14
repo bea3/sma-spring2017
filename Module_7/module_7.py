caaee32a204d3be78f37e54c99766157dedf8de2
Garcia, Beatrice <Beatrice.Garcia@jhuapl.edu>
2017-03-13 20:03:04 -0400

    stuff

--- Module_7/module_7.py
+++ Module_7/module_7.py
@@ -11,7 +11,10 @@ your data?
 
 
 import praw
+import networkx as nx
+import matplotlib.pyplot as plt
 import csv
+import operator
 
 
 def get_data():
@@ -28,7 +31,7 @@ def get_data():
     subreddit = reddit.subreddit('climbing')
 
     with open('data.csv', 'wb') as csvfile:
-        writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(['forum_id', 'submission_id', 'comment_id', 'user_id', 'user'])
 
         submissions = subreddit.submissions()
@@ -38,8 +41,57 @@ def get_data():
                 writer.writerow(info)
 
 
+def visualize_data():
+    G = nx.Graph()
+
+    # read csv and add edges
+    with open('data.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            G.add_edge(row[0].strip(), row[1].strip())
+            G.add_edge(row[1].strip(), row[2].strip())
+
+    # calculate betweenness centraliy
+    betweenness = nx.betweenness_centrality(G, endpoints=True)
+
+    # sort by greatest score to least
+    sorted_betweenness = sorted(betweenness.items(), key=operator.itemgetter(1))
+
+    labels = {}
+    other_nodes = {}
+    central_nodes = {}
+    count = 0
+
+    # separate nodes from the top 5 most central nodes and the rest and get the labels
+    for x in range(G.number_of_nodes()):
+        node = sorted_betweenness[x]
+        node_name = node[0]
+        node_score = node[1]
+        labels[node_name] = node_name
+        central_nodes[node_name] = node_score
+        count += 1
+
+    # positions for all nodes
+    pos = nx.spring_layout(G)
+
+    # draw labels
+    nx.draw_networkx_labels(G, pos, labels, font_size=14, font_family='sans-serif')
+
+    # draw the most central nodes, the rest of the nodes, and the edges
+    nx.draw_networkx_nodes(G, pos, nodelist=central_nodes.keys(), node_color='b',
+                           node_size=[v * 1000 for v in central_nodes.values()])
+    nx.draw_networkx_nodes(G, pos, nodelist=other_nodes.keys(), node_size=[v * 1000 for v in other_nodes.values()])
+    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+
+    # save and display graph
+    plt.axis('off')
+    plt.savefig("garcia_hw7.png")
+    plt.show()
+
 def main():
-    get_data()
+    # get_data()
+    visualize_data()
+
 
 if __name__ == "__main__": main()
 
