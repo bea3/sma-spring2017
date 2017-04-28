d1bd2cc4a540a26de771ec950934fd2634536117
Garcia, Beatrice <Beatrice.Garcia@jhuapl.edu>
2017-04-28 17:03:23 -0400

    gotta get replies

--- project/analysis/Bar_Charts.py
+++ project/analysis/Bar_Charts.py
@@ -1,10 +1,69 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import json
+import re
+import os
+import pprint
 
 # data to plot
 n_groups = 11
-rt_count = (90, 55, 40, 65, 90, 55, 40, 65, 90, 55, 40)
-fave_count = (85, 62, 54, 20, 90, 55, 40, 65, 90, 55, 40)
+# rt_count = (90, 55, 40, 65, 90, 55, 40, 65, 90, 55, 40)
+fave_count = (773, 62, 54, 20, 90, 55, 40, 65, 90, 55, 40)
+
+keyword = ['RBoKNShF4R',
+           'SEO4CUuzjq',
+           'O0xVQdZDkw',
+           'ENPlJ9KnZc',
+           'dhMehoWjdi',
+           'BIgjUQMqYu',
+           'MDsxTHHA1V',
+           'C8HrSJjeFw',
+           'd7OGHbmu71',
+           '6kE6GvulfI',
+           '5BjokX7HQ5']
+
+all_url_dicts = dict()
+
+for x in keyword:
+    all_url_dicts[x] = 0
+
+dir = os.path.dirname(__file__)
+filename = os.path.join(dir, '../data/tweets.json')
+with open(filename) as data_file:
+    df = json.load(data_file)
+    for data in df:
+        # get tweet text
+        text = data['text']
+        # get urls in text
+        regex_pattern = re.compile('https:\/\/t.co\/.*')
+        matches = re.findall(regex_pattern, text)
+        if (len(matches) > 0):
+            matches = matches[0].split()
+            # for each url, find the appropriate dictionary
+            for i in range(len(matches)):
+                url = matches[i]
+                if i == 0 or url.startswith("https://t.co/"):
+                    url = url.replace("https://t.co/", '')
+                    if url in all_url_dicts:
+                        all_url_dicts[url] += 1
+        else:
+            pass
+
+pprint.pprint(all_url_dicts)
+
+rt_count = (all_url_dicts['RBoKNShF4R'],
+           all_url_dicts['SEO4CUuzjq'],
+           all_url_dicts['O0xVQdZDkw'],
+           all_url_dicts['ENPlJ9KnZc'],
+           all_url_dicts['dhMehoWjdi'],
+           all_url_dicts['BIgjUQMqYu'],
+           all_url_dicts['MDsxTHHA1V'],
+           all_url_dicts['C8HrSJjeFw'],
+           all_url_dicts['d7OGHbmu71'],
+           all_url_dicts['6kE6GvulfI'],
+           all_url_dicts['5BjokX7HQ5'])
+
+
 
 # create plot
 fig, ax = plt.subplots()
---
+++ project/retrieve-data/Replies.py
@@ -0,0 +1,13 @@
+import tweepy
+
+consumer_key = "FhiyBRb0cz4zOoDfcHNVOyBDS"
+consumer_secret = "Q7MsbrPbFVzrwztW2KFH5JDGVpnSMhBlztwfJrLQeIGG591LST"
+access_token = "755868887300780032-FqY1RAUiznqmVMToIotldxavDEKdEe9"
+access_secret = "DV3O14tD9nTR1cVThyY1wriLMarXQoNQoudbZMHnUxYBP"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+api.
